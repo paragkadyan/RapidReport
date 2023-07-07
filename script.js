a20b7c3e317d9010b57630acf11792d346efecbf
Parag <kadyanparag@gmail.com>
2023-07-08 02:32:58 +0530

    added news API through JS

---
+++ script.js
@@ -0,0 +1,30 @@
+const API_key = "8d42d033147a4617a140dd8032065c3d";
+const url = "https://newsapi.org/v2/everything?q=";
+
+window.addEventListener('load', () => fetchnews("India"));
+
+async function fetchnews(query) {
+    const res = await fetch(`${url}${query}&apikey=${API_key}`);
+    const data = await res.json();
+    console.log(data);
+    bindData(data.articles);
+}
+
+function bindData(articles){
+    const cardsouter = document.getElementById("cards-outer");
+    const newsTemplate = document.getElementById("news-template");
+    cardsouter.innerHTML = "";
+
+    articles.forEach(article => {
+        if (!article.urlToImage) return;
+        const cardclone = newsTemplate.content.cloneNode(true);
+        cardclone.querySelector("img").src = article.urlToImage;
+        cardclone.querySelector("#new-title").textContent = article.title;
+
+        const formattedDate = new Date(article.publishedAt).toLocaleString('en-US', { timeZone: 'Asia/Kolkata' });
+        cardclone.querySelector("#news-source").textContent = `${article.source.name}, ${formattedDate}`;
+
+        cardclone.querySelector("#news-content").textContent = article.description;
+        cardsouter.appendChild(cardclone);
+    });
+}
